ffb750fabc488a7fcfcd49d9a698466999b821c9
Nina NJul <nina.dev.web@gmail.com>
2024-08-07 18:33:21 +0300

    feat: add lesson-4_homework

---
+++ ClassWork/Lesson-4/miscellaneous/script.js
@@ -0,0 +1,29 @@
+// console.log('first line');
+
+/*
+let sum = 10000;
+if (sum > 1000) {
+  // 1, [], {}, 'absdkl', "false", "0" - це true
+  // 0, -0, "", let x; Boolean(x); undefined;, null, false, NaN - false
+  console.log('знижка');
+  console.log('другий рядок');
+}
+  */
+
+// Треба ставити умову, яка завжди видає булевий результат, true або false
+
+let dayNumber = '';
+
+switch (dayNumber) {
+  case 0:
+    console.log('Понеділок');
+    break;
+  case 1:
+    console.log('Вівторок');
+    break;
+  case 2:
+    console.log('Середа');
+    break;
+  default:
+    console.log('Помилка');
+}

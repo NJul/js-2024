7d09a20da9e3ce2dba23fa8ba24fc5485fb140f0
Nina NJul <nina.dev.web@gmail.com>
2024-09-21 16:52:34 +0300

    feat: add lesson-9

---
+++ ClassWork/Lesson-9/script.js
@@ -0,0 +1,21 @@
+let myDiv = document.getElementById('myDiv');
+
+// myDiv.style.color = 'yellow';
+// myDiv.style.backgroundColor = 'green';
+// myDiv.style.padding = '10px';
+
+// myDiv.classList.add('myClass');
+
+// Подія - це сигнал, який повідомляє про наступ певного стану деякого об'єкта.
+// - кнопка натиснута
+// - курсор увійшов до області елемента
+// - на клавіатурі натиснута клавіша
+
+// Обробник події - функція, яка запускається у разі виникнення події.
+
+function changeColor() {
+  myDiv.classList.add('myClass');
+}
+
+// myDiv.onclick = changeColor;
+myDiv.addEventListener('click', changeColor);
